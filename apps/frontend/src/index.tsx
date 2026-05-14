4e62913b39c9ac06abca36e84342a4754460e85d
Akseli Kolari <akzuu@pm.me>
2026-05-14 11:25:23 +0300

    Setup sentry UI

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -4,10 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://9acf76e6b4d4bc9f51669d6366cf1307@o4511298353496064.ingest.de.sentry.io/4511387097104464",
+  sendDefaultPii: true,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Sentry.ErrorBoundary>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Sentry.ErrorBoundary>,
 );
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
@@ -5,5 +6,13 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), svgr()],
+
+  plugins: [react(), svgr(), sentryVitePlugin({
+    org: "tampereen-urheilusukeltajat-ry",
+    project: "tayttopaikka-ui"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 });
