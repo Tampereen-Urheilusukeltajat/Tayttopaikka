ef03ee95635e27b26f6719c509e33a6119b7f046
Akseli Kolari <akzuu@pm.me>
2026-08-29 13:06:16 +0300

    validation

--- apps/frontend/src/components/BlenderLogbook/validation.ts
+++ apps/frontend/src/components/BlenderLogbook/validation.ts
@@ -41,4 +41,43 @@ export const BLENDER_FILLING_EVENT_VALIDATION_SCHEMA = yup.object().shape({
       storageCylinderId: yup.string().required(FIELD_REQUIRED),
     }),
   ),
+  diluentFillingRows: yup.array().of(
+    yup.object().shape({
+      oxygenPercentage: yup
+        .number()
+        .typeError(FIELD_NUMBER)
+        .min(0)
+        .max(100)
+        .optional(),
+      heliumPercentage: yup
+        .number()
+        .typeError(FIELD_NUMBER)
+        .min(0)
+        .max(100)
+        .optional()
+        .when(['oxygenPercentage'], (oxygenPercentage, schema) =>
+          schema.max(
+            100 - (Number(oxygenPercentage) || 0),
+            'Happi- ja heliumprosenttien summa saa olla enintään 100',
+          ),
+        ),
+      startPressure: yup
+        .number()
+        .typeError(FIELD_NUMBER)
+        .min(0)
+        .required(FIELD_REQUIRED),
+      endPressure: yup
+        .number()
+        .typeError(FIELD_NUMBER)
+        .min(0)
+        .required(FIELD_REQUIRED)
+        .when(['startPressure'], (startPressure, schema) =>
+          schema.max(
+            startPressure as unknown as number,
+            'Loppupaine liian korkea',
+          ),
+        ),
+      storageCylinderId: yup.string().required(FIELD_REQUIRED),
+    }),
+  ),
 });
